7b593551a5de8e495867f73e015a0dc3141f1960
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-09-09 15:14:59 +0530

    NextJS CSS styles and Layout Added successfully.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,23 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/">
-        <h1>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat
-          ducimus minima possimus consequuntur? Facere porro voluptatum illum.
-          Dolore vitae, neque nisi omnis totam ducimus error iusto blanditiis
-          cupiditate voluptas. Quod?
-        </h1>
+    <div className={styles.container}>
+    <Featured/>
+    <CategoryList/>
 
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque culpa
-          saepe architecto quam a, quia sit iusto debitis fugit eos tempora
-          laudantium dignissimos odit quae quibusdam alias consequuntur quos
-          numquam ab voluptate, aut cum soluta, excepturi fugiat. Ratione,
-          libero aut, sequi facere illo dolorem ullam nesciunt consequuntur
-          sapiente, impedit ex? Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Reprehenderit culpa, magni quam et voluptates,
-          quibusdam harum vitae commodi nihil blanditiis sed consectetur ipsum
-          beatae vero! Distinctio rerum quasi sit at omnis porro incidunt hic
-          quos, cumque saepe fugiat doloribus soluta.
-        </p>
-      </Link>
+    <div className={styles.container}>
+      <CardList/>
+      <Menu/>
+    </div>
+
     </div>
   );
 }
---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+const CardList = () => {
+  return <div>CardList
+  
+  
+  <Pagination/></div>;
+};
+
+export default CardList;
